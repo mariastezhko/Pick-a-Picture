44bde16ad463be84e9847c349a7d48788e9c7f2b
Maria Stezhko <stezhko@gmail.com>
2018-02-10 21:10:41 -0800

    Clean up the code

--- apps/pictures_app/views.py
+++ apps/pictures_app/views.py
@@ -12,33 +12,6 @@ def index(request):
         return redirect('/')
     else:
         data = Image.objects.image_processor()
-        # images = []
-        # criteria = Criteria.objects.order_by('?').first()
-        # subcategories = random.sample(Subcategory.objects.filter(criteria=criteria),2)
-        # images_3 = random.sample(Image.objects.filter(subcategory=subcategories[0]),3)
-        # image_1 = random.sample(Image.objects.filter(subcategory=subcategories[1]),1)
-        # if image_1[0].subcategory.name == "wild_animals":
-        #     response = "Great Job! This is a wild animal, and other animals are domestic."
-        # elif image_1[0].subcategory.name == "domestic_animals":
-        #     response = "Great Job! This is a domestic animal, and other animals are wild."
-        # elif image_1[0].subcategory.name == "in_flight":
-        #     response = "Great Job! There is only one bird in flight, others are sitting on the branches."
-        # elif image_1[0].subcategory.name == "not_in_flight":
-        #     response = "Great Job! There is only one bird on the branch, others are flying."
-        # elif image_1[0].subcategory.name == "one_animal":
-        #     response = "Great Job! There is only one animal on this picture, and two animals on other pictures."
-        # elif image_1[0].subcategory.name == "two_animals":
-        #     response = "Great Job! There are two animals on this picture, and only one on other pictures."
-        # elif image_1[0].subcategory.name == "one_bird":
-        #     response = "Great Job! There is only one bird on this picture, and two birds on other pictures."
-        # elif image_1[0].subcategory.name == "two_birds":
-        #     response = "Great Job! There are two birds on this picture, and only one on other pictures."
-        #
-        # request.session['correct_answer'] = image_1[0].id
-        # correct_answer = image_1[0].id
-        #
-        # images = images_3 + image_1
-        # random.shuffle(images)
         return render(request, 'pictures_app/index.html', data)
 
 def renderForm(request):
